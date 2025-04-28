e8260eb956d78b20ada9d142107a68395d46883b
reigadegr <272268642@qq.com>
2025-04-29 01:48:56 +0800

    chore: 优化生成pem的命令

--- gen_EdDSA.sh
+++ gen_EdDSA.sh
@@ -1,5 +1,5 @@
 # 生成私钥
-openssl genpkey -algorithm ED25519 -out private_key.pem
+openssl genpkey -algorithm ED25519 -out keys/private_key.pem
 
 # 从私钥导出公钥
-openssl pkey -in private_key.pem -pubout -out public_key.pem
+openssl pkey -in keys/private_key.pem -pubout -out keys/public_key.pem

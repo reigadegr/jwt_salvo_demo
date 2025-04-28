e20c1317cfe7ecc1df69ddf7382d7a09981b2c4c
reigadegr <272268642@qq.com>
2025-04-28 16:15:09 +0800

    refactor: 重构代码结构

--- src/main.rs
+++ src/main.rs
@@ -7,48 +7,13 @@
 )]
 
 mod config;
+mod controller;
 mod exclusive;
 mod jwt;
 mod models;
-use exclusive::write_response::{render_error, render_success};
-use jwt::{generate_token, validate_token};
-use salvo::{http::StatusCode, prelude::*};
-use serde::Deserialize;
-use simd_json::json;
-use stringzilla::sz;
 
-#[derive(Deserialize)]
-struct LoginRequest {
-    username: String,
-    password: String,
-}
-
-#[handler]
-async fn login(req: &mut Request, res: &mut Response) {
-    let login_req: LoginRequest = req.parse_json().await.unwrap();
-    // 模拟用户验证
-    if login_req.username == "user1" && login_req.password == "password1" {
-        let token = generate_token(&login_req.username).unwrap();
-        return render_success(res, json!({ "token": token }), "成功生成token");
-    }
-    res.status_code(StatusCode::UNAUTHORIZED);
-    return render_error(res, "Invalid credentials");
-}
-
-#[handler]
-async fn profile(req: &mut Request, res: &mut Response) {
-    if let Some(token) = req.header("Authorization") {
-        let token: &str = token;
-        let token = token.split_whitespace().last().unwrap();
-        if let Ok(claims) = validate_token(token) {
-            return render_success(res, json!({ "user": claims  }), "成功获取用户信息");
-        }
-        res.status_code(StatusCode::FORBIDDEN);
-        return render_error(res, "Invalid token");
-    }
-    res.status_code(StatusCode::UNAUTHORIZED);
-    return render_error(res, "No token provided");
-}
+use controller::{jwt_auth, login, profile};
+use salvo::prelude::*;
 
 #[tokio::main]
 async fn main() {
@@ -60,28 +25,6 @@ async fn main() {
                 .push(Router::with_path("profile").get(profile)),
         );
 
-    let acceptor = TcpListener::new("127.0.0.1:3000").bind().await;
+    let acceptor = TcpListener::new("0.0.0.0:3000").bind().await;
     Server::new(acceptor).serve(router).await;
-}
-
-#[handler]
-async fn jwt_auth(req: &mut Request, res: &mut Response) {
-    let Some(token) = req.header("Authorization") else {
-        res.status_code(StatusCode::UNAUTHORIZED);
-        return render_error(res, "No token provided");
-    };
-
-    let token: &str = token;
-    #[cfg(debug_assertions)]
-    println!("{token}");
-
-    let Some(pos) = sz::find(token, " ") else {
-        res.status_code(StatusCode::UNAUTHORIZED);
-        return render_error(res, "Invalid token format");
-    };
-    let jwt_token = &token[pos + 1..];
-    if validate_token(jwt_token).is_err() {
-        res.status_code(StatusCode::FORBIDDEN);
-        return render_error(res, "Invalid token");
-    }
 }

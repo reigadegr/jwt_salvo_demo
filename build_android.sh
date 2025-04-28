57536cce3d95125b35099ec31a80ce7da63793fa
reigadegr <272268642@qq.com>
2025-04-28 13:46:50 +0800

    build: 添加部分构建脚本

---
+++ build_android.sh
@@ -0,0 +1,18 @@
+cargo fmt
+rm -rf $(find ./target/aarch64-linux-android/release -name "*jwt_salvo_demo*")
+
+export RUSTFLAGS="-C default-linker-libraries \
+-Z external-clangrt \
+-Z macro-backtrace \
+-Z remap-cwd-prefix=. \
+-Z dep-info-omit-d-target \
+-C target-feature=xeon \
+-C llvm-args=-enable-ml-inliner=release \
+-C llvm-args=-inliner-interactive-include-default \
+-C llvm-args=-ml-inliner-model-selector=arm64-mixed \
+-C llvm-args=-ml-inliner-skip-policy=if-caller-not-cold \
+-C link-args=-fomit-frame-pointer \
+-C link-args=-Wl,--icf=all,-z,relro,--pack-dyn-relocs=android+relr,-x,-s,--strip-all,-z,now
+" 
+
+cargo +nightly ndk -p 35 -t arm64-v8a build --target aarch64-linux-android -Z trim-paths -Z build-std --release --verbose  --
--- watch.sh
+++
@@ -1,1 +0,0 @@
-cargo-watch -x +nightly ndk -p 35 -t arm64-v8a run --target aarch64-linux-android

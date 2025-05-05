1b90c26c8c183aea08ddb59f93545ee6436d5923
reigadegr <272268642@qq.com>
2025-05-05 16:14:33 +0800

    fix: ab脚本出错

--- ab_test.sh
+++ ab_test.sh
@@ -1,2 +1,2 @@
 /data/data/com.termux/files/usr/bin/ab -n 20000 -c 10000 \
--T "application/json" -p data.json -H "Content-Type: application/json" -s 60 http://localhost:3000/login
+-T "application/json" -p data.json -s 60 http://localhost:3000/login

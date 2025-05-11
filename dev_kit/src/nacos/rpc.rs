743d6200345c479ab3bfecb0be7545ddbcc23d95
reigadegr <2722688642@qq.com>
2025-05-11 15:19:49 +0800

    fix: rpc没有被附加到模块

--- dev_kit/src/nacos/rpc.rs
+++ dev_kit/src/nacos/rpc.rs
@@ -1,9 +1,6 @@
-use crate::config::get_cfg;
 use anyhow::Result;
 use nacos_sdk::api::naming::ServiceInstance;
-use super::naming_service::{MyNamingEventListener, get_naming_service};
-use service_instance::get_service_instance;
-use std::sync::Arc;
+use super::naming_service::get_naming_service;
 
 async fn get_healthy_instance(
     service_name: &str,

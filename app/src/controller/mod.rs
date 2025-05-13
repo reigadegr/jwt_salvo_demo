ddf5a8adf21b9b53e81b55c4c250db33f48611f8
reigadegr <272268642@qq.com>
2025-05-14 02:46:26 +0800

    feat: 设置超时强制停机时间

--- app/src/controller/mod.rs
+++ app/src/controller/mod.rs
@@ -9,7 +9,7 @@ pub async fn graceful_stop(req: &Request, res: &mut Response) {
     let time = req.param::<u64>("secs").unwrap();
     tokio::spawn(async move {
         tokio::time::sleep(std::time::Duration::from_secs(time)).await;
-        get_handle().stop_graceful(None);
+        get_handle().stop_graceful(std::time::Duration::from_secs(60));
     });
     render_success(res, "开始停止接收请求", "OK");
 }

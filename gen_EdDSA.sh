cdbfdb3c3d77506f37d001794e383ce8836ea7b9
reigadegr <2722688642@qq.com>
2026-01-14 17:24:01 +0800

    fix: change gen_eddsa script to new path

--- gen_EdDSA.sh
+++ gen_EdDSA.sh
@@ -2,4 +2,4 @@
 openssl genpkey -algorithm ED25519 -out app/keys/private_key.pem
 
 # 从私钥导出公钥
-openssl pkey -in dev_kit/keys/private_key.pem -pubout -out app/keys/public_key.pem
+openssl pkey -in app/keys/private_key.pem -pubout -out app/keys/public_key.pem

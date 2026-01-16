130cfc7f293e60a991be51fcf98a59bd80781936
reigadegr <2722688642@qq.com>
2026-01-16 22:06:08 +0800

    refactor: sync with my project

--- crates/server_handle/src/lib.rs
+++ crates/server_handle/src/lib.rs
@@ -21,7 +21,8 @@ use chrono::Local;
 use my_config::config::get_cfg;
 use salvo::{conn::tcp::TcpAcceptor, prelude::*};
 use shutdown_handle::{init_handle, shutdown_signal};
-use std::fmt;
+use std::{fmt, time::Duration};
+use tracing::Level;
 use tracing_subscriber::fmt::{format::Writer, time::FormatTime};
 
 struct LoggerFormatter;
@@ -32,7 +33,7 @@ impl FormatTime for LoggerFormatter {
     }
 }
 
-pub async fn use_http1() -> TcpAcceptor {
+pub async fn use_http1() -> Server<TcpAcceptor> {
     let ip = &get_cfg().client_cfg.service_ip;
     let port = get_cfg().client_cfg.service_port;
     let listen_addr = format!("{ip}:{port}");
@@ -41,17 +42,36 @@ pub async fn use_http1() -> TcpAcceptor {
         "📖 Open API Page: http://{}/scalar",
         listen_addr.replace("0.0.0.0", "127.0.0.1")
     );
-    TcpListener::new(listen_addr).bind().await
+
+    let acceptor = TcpListener::new(listen_addr).bind().await;
+
+    let mut server = Server::new(acceptor);
+    server
+        .http1_mut()
+        .keep_alive(true)
+        .header_read_timeout(Some(Duration::from_secs(5)));
+
+    server
 }
+
 pub fn shutdown_signal_monitor_init() {
     tokio::spawn(shutdown_signal());
 }
 
 pub async fn init_server() -> Server<TcpAcceptor> {
-    tracing_subscriber::fmt().with_timer(LoggerFormatter).init();
+    let log_level = if cfg!(debug_assertions) {
+        Level::DEBUG
+    } else {
+        Level::INFO
+    };
+
+    tracing_subscriber::fmt()
+        .with_timer(LoggerFormatter)
+        .with_max_level(log_level)
+        .init();
+
     let () = shutdown_signal_monitor_init();
-    let acceptor = use_http1().await;
-    let server = Server::new(acceptor);
+    let server = use_http1().await;
     init_handle(server.handle());
     tokio::spawn(shutdown_signal());
     server
